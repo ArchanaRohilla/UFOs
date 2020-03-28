cc84b21dfd48b1a6ebfc0312bc5343098cfdf015
ArchanaRohilla <ar.archana.ar@gmail.com>
2020-03-27 23:32:51 -0700

    Adding updated app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -23,5 +23,25 @@ function buildTable(data) {
     });        
 }
 
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+        // apply 'filter' to the table data to only keep the
+        // rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // rebuid the table using the filtered data
+    // @note: if no data was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+// attach an event to listen for the form button 
+d3.selectAll("#filter-btn").on("click", handleClick);
+// build the table when the page loads
+buildTable(tableData);
 
 
